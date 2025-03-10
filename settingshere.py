c83ec0e4e68e6b49c5723bdbdc6f5556d999254b
KKNg0-0 <kkng215@gmail.com>
2025-03-10 23:09:05 +0900

    Update: Removed 'else' line

--- settingshere.py
+++ settingshere.py
@@ -115,11 +115,9 @@ def run_bot():
                     current_title = playlists[current_song-1]['title']
                     newmsg = "I am playing " + current_title
                     await message.reply(newmsg)
-                else:
-                    await message.reply("5 Ringgit for 1 search")
 
             except Exception as e:
-                print("search that urself bruh")
+                await message.reply("5 Ringgit for 1 search")
                 
         if message.content.startswith("!pause"):
             try:
